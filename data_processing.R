e486fececed86db9fc4737f58362ceb5ff4ba33f
Gabriel <gabriel.dellaccio@gmail.com>
2018-04-12 00:32:49 -0400

    Completed processing for within behavior results

--- data_processing.R
+++ data_processing.R
@@ -41,19 +41,6 @@ boidDataCollection <- c(boid_25.data, boid_5.data, boid_75.data, boid_100.data)
 
 
 
-
-# determineAverage <- function(dataFileArray) {
-#   meansArray <- c()
-#   temp <- 1
-#   for(gsiLog in dataFileArray) {
-#     currentMean <- mean(gsiLog)
-#     meansArray[temp] <- currentMean
-#     temp <- temp + 1
-#   }
-#   return(meansArray)
-# }
-
-
 returnTrialAvg <- function(dataEx, whichPhase) {
   overallAvg <- NA
   stdev <- NA
@@ -79,23 +66,6 @@ returnTrialAvg <- function(dataEx, whichPhase) {
 
 
 
-# returnGSI <- function(dataFile, whichPhase) {
-#   if(whichPhase == "first") {
-#     tempGSI <- dataFile$GSI[1:200]
-#   } else if(whichPhase == "second") {
-#     tempGSI <- dataFile$GSI[200:800]
-#   } else if(whichPhase == "full") {
-#     tempGSI <- dataFile$dataFile$GSI[1:800]
-#   }
-#   return(tempGSI)
-# }
-  
-  error.bar <- function(x, y, upper, lower=upper, length=0.1,...){
-   if(length(x) != length(y) | length(y) !=length(lower) | length(lower) != length(upper))
-   stop("vectors must be same length")
-   arrows(x,y+upper, x, y-lower, angle=90, code=3, length=length, ...)
-  }
-
 createBehaviorRangeGraph <- function(point25, std25, point50, std50, point75, std75, point100, std100, behaviorTitle) {
 
   # print(c(point25,point50, point75, point100))
@@ -113,68 +83,16 @@ createBehaviorRangeGraph <- function(point25, std25, point50, std50, point75, st
                     width=.2) +
       xlab('Strength') +
       ylab('GSI') + ylim(0.00:1.00) + ggtitle(behaviorTitle)
+
+
+      # ggplot(mean_sd, aes(x=group, y=mean)) + 
+      # geom_line(stat='identity') +
+      # geom_errorbar(aes(ymin=mean-sem, ymax=mean+sem),
+      #               width=.2) +
+      # xlab('Strength') +
+      # ylab('GSI') + ylim(0.00:1.00) + ggtitle(behaviorTitle)
    
 
- 
-
-
-  # tempStrengthData <- data.frame(
-  #   plotD <- pointsLog,
-  #   plotDstd <- stdLog,
-  #   # Strength_.25 <- point25,
-  #   # Strengh_.50 <- point50,
-  #   # Strength_.75 <- point75,
-  #   # Strength_1.0 <- point100,
-  #   gsi <- c(1,2,3,4,5,6,7,8,10) ,
-  #   Strength <- c(25,50,75,100)
-  #   )
-
-
-  # strengthMag <- ggplot(tempStrengthData, aes(Strength, y <- GSI)) + 
-  # geom_line(aes(y <- c(point25,point50, point75, point100)), size =2)   
-  
-
-  # points <- c(point25,point50, point75, point100)
-  # tempPlot <- plot(points, type="o", col="blue", xlab="Behavior Strength", ylab="GSI", axes=FALSE, main=behaviorTitle, ylim=c(0.:1.0))
-  # axis(1, at=1:4, lab=c("25","50","75","100"))
-  # axis(2, las=1, at=10.0*((1:100)/100))
-  # error.bar(tempPlot, pointsLog, stdLog)
-  # plotReturn <- ggplot(tempStrengthData, aes(x<- Strength,y<-gsi)) +
-          # geom_line(aes(plotD), size=2)
-        # geom_line(aes(y <- plotD), size = 2) 
-    # geom_errorbar() + aes(ymin = plotD - plotDstd, ymax = plotD + ymax)
-
-# GSI_behaviorVal5 <-
-#   data.frame(
-#     AVOID <- GSI_avoid5,
-#     ALIGN <- GSI_align5,
-#     BOID <- GSI_boid5,
-#     LIGHT <- GSI_attract5,
-#     ATTRACTION <- GSI_attract5,
-#     Time <- 1:800
-#   )
-
-
-
-# GSIbehaviors5 <- ggplot(GSI_behaviorVal5, aes(Time, y <- GSI)) + 
-#   geom_line(aes(y <- GSI_avoid5, colour <- "AVOIDANCE"), size =2) +  
-#   geom_line(aes(y <- GSI_align5, colour <- "ALIGNMENT"), size =2)+ 
-#   geom_line(aes(y <- GSI_boid5, colour <- "BOID"), size =2) +
-#   geom_line(aes(y <- GSI_attract5, colour <- "ATTRACTION"), size =2) +
-#   geom_line(aes(y <- GSI_light5, colour <- "LIGHT"), size =2)
-
-# GSIbehaviors5 + ggtitle("Behavior Stabilities")
-
-
-  # plotReturn
-  # title(main=behaviorTitle)
-  # axis(1, at=1:4, )
-  # geom_line(aes(y <- GSI_align5, colour <- "ALIGNMENT"), size =2)+ 
-  # geom_line(aes(y <- GSI_boid5, colour <- "BOID"), size =2) +
-  # geom_line(aes(y <- GSI_attract5, colour <- "ATTRACTION"), size =2) +
-  # geom_line(aes(y <- GSI_light5, colour <- "LIGHT"), size =2)
-
-  # strengthMag + ylim(0.0,1.0)
 
 }
 
@@ -185,10 +103,18 @@ createBehaviorRangeGraph <- function(point25, std25, point50, std50, point75, st
 
 
 
+
+
 graphGSIByStrength <- function(whichPhase) {
 
   phase <- whichPhase
+
+
   # print(paste("THIS IS PHASE", phase))
+
+
+
+
 
   light25result <- returnTrialAvg(light_25.data, phase)
   light50result <- returnTrialAvg(light_5.data, phase)
@@ -225,18 +151,15 @@ graphGSIByStrength <- function(whichPhase) {
 
   # boidPlotData <- c(boid25result, boid50result, boid75result, boid100result)
 
-  # createBehaviorRangeGraph(light25result[1], light25result[2], light50result[1], light50result[2], light75result[1], light75result[2], light100result[1], light100result[2], "Light Attraction")
 
-  # png(filename="Std_PNG.png", 
-  #     units="in", 
-  #     width=5, 
-  #     height=4, 
-  #     pointsize=12, 
-  #     res=72)
-  # my_sc_plot(data)
-  # dev.off()
 
-  
+  # str25, str50,str75, str100)
+  # print(y)
+
+
+
+
+  lightPlot <<- createBehaviorRangeGraph(light25result[1], light25result[2], light50result[1], light50result[2], light75result[1], light75result[2], light100result[1], light100result[2], "Light Attraction")
   alignPlot <<- createBehaviorRangeGraph(align25result[1], align25result[2], align50result[1], align50result[2], align75result[1], align75result[2], align100result[1], align100result[2], "Social Alignment")
   attractionPlot <<- createBehaviorRangeGraph(attraction25result[1], attraction25result[2], attraction50result[1], attraction50result[2], attraction75result[1], attraction75result[2], attraction100result[1], attraction100result[2], "Social Attraction")
   avoidPlot <<- createBehaviorRangeGraph(avoidance25result[1], avoidance25result[2], avoidance50result[1], avoidance50result[2], avoidance75result[1], avoidance75result[2], avoidance100result[1], avoidance100result[2], "Social Avoidance")
@@ -244,19 +167,127 @@ graphGSIByStrength <- function(whichPhase) {
 
 }
 
-# graphGSIByStrength("first")
+graphGSIByStrength("first")
 # graphGSIByStrength("first")
 # graphGSIByStrength("first")
 # graphGSIByStrength("first")
 
-graphGSIByStrength("second")
 # graphGSIByStrength("second")
 # graphGSIByStrength("second")
 # graphGSIByStrength("second")
-alignPlot
-attractionPlot
-avoidPlot
-boidPlot
+# graphGSIByStrength("second")
+# alignPlot
+# attractionPlot
+# avoidPlot
+# boidPlot
+# lightPlot
+
+
+calculateWithinStats <- function(file25, file50, file75, file100, title, whichPhase) {
+
+
+
+      
+
+      # length(temp25)
+
+      if(whichPhase == "first") {
+        temp25 <- c(file25$Trial1[1:200], file25$Trial3[1:200], file25$Trial3[1:200])
+        temp50 <- c(file50$Trial1[1:200], file50$Trial3[1:200], file50$Trial3[1:200])
+        temp75 <- c(file75$Trial1[1:200], file75$Trial3[1:200], file75$Trial3[1:200])
+        temp100 <- c(file100$Trial1[1:200], file100$Trial3[1:200], file100$Trial3[1:200])
+       
+        tempLabel <- c(rep("str25",600),rep("str50",600),rep("str75", 600),rep("str100",600))
+        tempTotal <- c(temp25[1:600],temp50[1:600],temp75[1:600],temp100[1:600])
+
+      } else if (whichPhase =="second") {
+        temp25 <- c(file25$Trial1[200:800], file25$Trial3[200:800], file25$Trial3[200:800])
+        temp50 <- c(file50$Trial1[200:800], file50$Trial3[200:800], file50$Trial3[200:800])
+        temp75 <- c(file75$Trial1[200:800], file75$Trial3[200:800], file75$Trial3[200:800])
+        temp100 <- c(file100$Trial1[200:800], file100$Trial3[200:800], file100$Trial3[200:800])
+        
+
+         tempLabel <- c(rep("str25",1800),rep("str50",1800),rep("str75", 1800),rep("str100",1800))
+        tempTotal <- c(temp25[1:1800],temp50[1:1800],temp75[1:1800],temp100[1:1800])
+
+      } else if( whichPhase =="full") {
+
+        temp25 <- c(file25$Trial1, file25$Trial3, file25$Trial3)
+        temp50 <- c(file50$Trial1, file50$Trial3, file50$Trial3)
+        temp75 <- c(file75$Trial1, file75$Trial3, file75$Trial3)
+        temp100 <- c(file100$Trial1, file100$Trial3, file100$Trial3)
+
+
+        tempLabel <- c(rep("str25",2400),rep("str50",2400),rep("str75", 2400),rep("str100",2400))
+        tempTotal <- c(temp25[1:2400],temp50[1:2400],temp75[1:2400],temp100[1:2400])
+      }
+     
+      # length(tempTotal)
+
+      lightStats <- data.frame(
+        # str25 = temp25[1:2400],
+        # str50 = temp50[1:2400],
+        # str75 = temp75[1:2400],
+        # str100 = temp100[1:2400],
+        type = tempLabel,
+        gsiList = tempTotal
+      )
+
+
+    fit <- aov(gsiList ~ type, data=lightStats)
+
+    txtSumName <- paste("./",title,"_Anova_summary.txt")
+    txtResultsName <- paste("./",title,"_Anova_results.txt")
+    txtPostHocName <- paste("./",title,"_Anova_PostHoc.txt")
+
+    capture_a <- summary(fit)
+    capture.output(capture_a, file = txtSumName)
+    capture_b <- fit
+    capture.output(capture_b, file = txtResultsName)
+    capture_c <- fit
+    capture.output(capture_c, file = txtPostHocName)
+
+    postHoc <- TukeyHSD(fit)
+
+    fit
+    postHoc
+    graphed <- plot(postHoc)
+    
+    graphed 
+    
+    # fileLoc <- "./results.txt"
+    # toWrite <- ""
+    # #sumOfStats <- summary(fit)
+    # toWrite <- paste(toWrite, fit , "\t")
+
+    # toWrite <- paste(toWrite,model.tables(fit,"means"),"\t")
+    #print(model.tables(fit,"effects"))
+    # write.table(toWrite, fileLoc, sep="\t")
+}
+
+
+# calculateAcrossStats
+
+      
+      # print(temp25)
+
+
+    
+calculateWithinStats(alignment_25.data, alignment_5.data, alignment_75.data, alignment_100.data, "alignStatsResults", "first")
+
+calculateWithinStats(attraction_25.data, attraction_5.data, attraction_75.data, attraction_100.data, "attractionStatsResults", "first")
+
+calculateWithinStats(avoidance_25.data, avoidance_5.data, avoidance_75.data, avoidance_100.data, "avoidanceStatsResults", "first")
+
+calculateWithinStats(light_25.data, light_5.data, light_75.data, light_100.data, "lightStatsResults", "first")
+
+calculateWithinStats(boid_25.data, boid_5.data, boid_75.data, boid_100.data, "boidStatsResults", "first")
+    # y <- A
+    # One Way Anova (Completely Randomized Design)
+    
+# graphGSIByStrength("second")
+
+# lightPlot
 
 
 
